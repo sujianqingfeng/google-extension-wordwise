b64bac0722956c61cbea66837cd957e032145ecb
hens <121116111@qq.com>
2024-03-06 14:24:13 +0800

    style(expand): icon color

--- src/entries/query/TranslateWord/Expand.tsx
+++ src/entries/query/TranslateWord/Expand.tsx
@@ -24,7 +24,7 @@ export default function Expand(props: ExpandProps) {
       )}
 
       <div className="p-1 flex justify-end bg-gray-100 dark:bg-slate-400/10">
-        <MdExpandMore className="cursor-pointer" onClick={toggle} />
+        <MdExpandMore className="cursor-pointer dark:text-gray-400 text-black" onClick={toggle} />
       </div>
     </div>
   )
