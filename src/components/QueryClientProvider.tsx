f625ac085980fcace1d0d4ac7a711f819e43e22f
hens <121116111@qq.com>
2024-07-21 01:11:30 +0800

    chore: retry count

--- src/components/QueryClientProvider.tsx
+++ src/components/QueryClientProvider.tsx
@@ -2,7 +2,13 @@ import { QueryClient } from "@tanstack/react-query"
 import { QueryClientProvider as ReactQueryClientProvider } from "@tanstack/react-query"
 import type { ReactNode } from "react"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: 1,
+		},
+	},
+})
 
 export default function QueryClientProvider({
 	children,
