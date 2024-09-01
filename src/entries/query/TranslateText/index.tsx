8afac29843412e33fec49b6f87bb5c2265427022
hens <121116111@qq.com>
2024-09-01 23:55:38 +0800

    feat: copy text in translate text panel

--- src/entries/query/TranslateText/index.tsx
+++ src/entries/query/TranslateText/index.tsx
@@ -1,6 +1,6 @@
 import Loading from "@/components/Loading"
 import { ErrorBoundary, type FallbackProps } from "react-error-boundary"
-import { WandSparkles, Volume2 } from "lucide-react"
+import { WandSparkles, Volume2, Copy } from "lucide-react"
 import { useSuspenseQuery } from "@tanstack/react-query"
 import { createBackgroundMessage } from "@/messaging/background"
 import { onBackgroundMessage, sendBackgroundMessage } from "@/messaging/content"
@@ -8,92 +8,100 @@ import { useEffect, useRef, useState } from "react"
 import Analyze from "./Analyze"
 
 type TranslateTextProps = {
-  text: string
+	text: string
 }
 
 const bgs = createBackgroundMessage()
 
 function TranslateText({ text }: TranslateTextProps) {
-  const { data: translateResult } = useSuspenseQuery({
-    queryKey: ["translate", text],
-    queryFn: () => bgs.fetchAiTranslate({ text, provider: "deepSeek" }),
-  })
-  const [analyzeResult, setAnalyzeResult] = useState("")
-  const [analyzeLoading, setAnalyzeLoading] = useState(false)
-  const removeCallback = useRef<() => void>()
+	const { data: translateResult } = useSuspenseQuery({
+		queryKey: ["translate", text],
+		queryFn: () => bgs.fetchAiTranslate({ text, provider: "deepSeek" }),
+	})
+	const [analyzeResult, setAnalyzeResult] = useState("")
+	const [analyzeLoading, setAnalyzeLoading] = useState(false)
+	const removeCallback = useRef<() => void>()
 
-  const onAnalyze = async () => {
-    setAnalyzeLoading(true)
-    sendBackgroundMessage("analyzeGrammar", text)
-  }
+	const onAnalyze = async () => {
+		setAnalyzeLoading(true)
+		sendBackgroundMessage("analyzeGrammar", text)
+	}
 
-  const onSystemTTS = () => {
-    const msg = new SpeechSynthesisUtterance(text)
-    msg.lang = "en-GB"
-    msg.rate = 0.6
-    msg.volume = 1
-    window.speechSynthesis.speak(msg)
-  }
+	const onSystemTTS = () => {
+		const msg = new SpeechSynthesisUtterance(text)
+		msg.lang = "en-GB"
+		msg.rate = 0.6
+		msg.volume = 1
+		window.speechSynthesis.speak(msg)
+	}
 
-  useEffect(() => {
-    if (analyzeLoading) {
-      removeCallback.current = onBackgroundMessage(
-        "analyzeGrammarResult",
-        ({ data: { result, done } }) => {
-          setAnalyzeResult(result)
-          setAnalyzeLoading(!done)
-        },
-      )
-    }
+	const onCopy = () => {
+		navigator.clipboard.writeText(text)
+	}
 
-    return () => {
-      removeCallback.current?.()
-    }
-  }, [analyzeLoading])
+	useEffect(() => {
+		if (analyzeLoading) {
+			removeCallback.current = onBackgroundMessage(
+				"analyzeGrammarResult",
+				({ data: { result, done } }) => {
+					setAnalyzeResult(result)
+					setAnalyzeLoading(!done)
+				},
+			)
+		}
 
-  return (
-    <div className="text-[12px] font-normal dark:text-gray-400 text-black">
-      <div className="px-2 pb-2">
-        <div className="mt-2">{text}</div>
-        <div className="mt-2">{translateResult}</div>
-      </div>
-      <Analyze result={analyzeResult} />
-      <div className="p-2 flex justify-end bg-gray-100 dark:bg-slate-400/10 gap-2">
-        <Volume2
-          onClick={onSystemTTS}
-          size={14}
-          className="cursor-pointer dark:text-gray-400 text-black"
-        />
+		return () => {
+			removeCallback.current?.()
+		}
+	}, [analyzeLoading])
 
-        {analyzeLoading ? (
-          <Loading size={14} />
-        ) : (
-          <WandSparkles
-            size={13}
-            className="cursor-pointer dark:text-gray-400 text-black"
-            onClick={onAnalyze}
-          />
-        )}
-      </div>
-    </div>
-  )
+	return (
+		<div className="text-[12px] font-normal dark:text-gray-400 text-black">
+			<div className="px-2 pb-2">
+				<div className="mt-2">{text}</div>
+				<div className="mt-2">{translateResult}</div>
+			</div>
+			<Analyze result={analyzeResult} />
+			<div className="p-2 flex justify-end bg-gray-100 dark:bg-slate-400/10 gap-[10px]">
+				<Copy
+					onClick={onCopy}
+					size={14}
+					className="cursor-pointer dark:text-gray-400 text-black"
+				/>
+				<Volume2
+					onClick={onSystemTTS}
+					size={14}
+					className="cursor-pointer dark:text-gray-400 text-black"
+				/>
+				{analyzeLoading ? (
+					<Loading size={14} />
+				) : (
+					<WandSparkles
+						size={13}
+						className="cursor-pointer dark:text-gray-400 text-black"
+						onClick={onAnalyze}
+					/>
+				)}
+			</div>
+		</div>
+	)
 }
 
 function fallbackRender({ error }: FallbackProps) {
-  return (
-    <div className="p-2">
-      <p>Something went wrong:</p>
-      <pre className="text-red">{error.message}</pre>
-    </div>
-  )
+	return (
+		<div className="p-2">
+			<p>Something went wrong:</p>
+			<pre className="text-red">{error.message}</pre>
+		</div>
+	)
 }
 
 function TranslateTextErrorWrapper({ text }: TranslateTextProps) {
-  return (
-    <ErrorBoundary fallbackRender={fallbackRender}>
-      <TranslateText text={text} />
-    </ErrorBoundary>
-  )
+	return (
+		<ErrorBoundary fallbackRender={fallbackRender}>
+			<TranslateText text={text} />
+		</ErrorBoundary>
+	)
 }
 
 export default TranslateTextErrorWrapper
--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,44 +1,44 @@
 export function isEnglishText(text: string) {
-  const englishTextRegex = /^[A-Za-z\s.,!?;_:'"-=\u2019]*$/
-  return englishTextRegex.test(text)
+	const englishTextRegex = /^[A-Za-z\s.,!?;_:'"-=\>\u2019]*$/
+	return englishTextRegex.test(text)
 }
 
 export function similarPascalCase(str: string): boolean {
-  const customPatternRegex = /^[A-Z][a-zA-Z]*[A-Z][a-zA-Z]*$/
-  return customPatternRegex.test(str)
+	const customPatternRegex = /^[A-Z][a-zA-Z]*[A-Z][a-zA-Z]*$/
+	return customPatternRegex.test(str)
 }
 
 export function similarCamelCase(str: string) {
-  const customPatternRegex = /^[a-z]+[A-Z][a-zA-Z0-9]*$/
-  return customPatternRegex.test(str)
+	const customPatternRegex = /^[a-z]+[A-Z][a-zA-Z0-9]*$/
+	return customPatternRegex.test(str)
 }
 
 export function isText(text: string) {
-  const trimText = text.trim()
+	const trimText = text.trim()
 
-  if (similarCamelCase(trimText) || similarPascalCase(trimText)) {
-    return true
-  }
+	if (similarCamelCase(trimText) || similarPascalCase(trimText)) {
+		return true
+	}
 
-  return [' ', '.', ',', '!', '?', ';', ':'].some((char) =>
-    trimText.includes(char)
-  )
+	return [" ", ".", ",", "!", "?", ";", ":"].some((char) =>
+		trimText.includes(char),
+	)
 }
 
 export function breakChar(char: string) {
-  return [
-    ' ',
-    ',',
-    '.',
-    '!',
-    '?',
-    ';',
-    ':',
-    '(',
-    ')',
-    '[',
-    ']',
-    '{',
-    '}'
-  ].includes(char)
+	return [
+		" ",
+		",",
+		".",
+		"!",
+		"?",
+		";",
+		":",
+		"(",
+		")",
+		"[",
+		"]",
+		"{",
+		"}",
+	].includes(char)
 }
